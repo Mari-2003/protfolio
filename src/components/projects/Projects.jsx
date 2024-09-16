905d167c78a1b2b32e351cd2e7c0fff396d73c90
Mari-2003 <marinamachivayam2003@gmail.com>
2024-09-16 12:15:44 +0530

    add project and responsive changes is done

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Projects.css'
-import architect from '../../assets/architect.png'
+import architect from '../../assets/architect.png';
+import school from "../../assets/school Management.png";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
 export const Projects = () => {
@@ -26,6 +27,22 @@ export const Projects = () => {
             <div className='arrowPostion'>
 
             <a target='blank' href='https://ahd-ui.onrender.com/home'><FaArrowAltCircleRight className='image-fluid projectArrow'/></a>
+            </div>
+          </div>
+          <div className='col-xl-4 col-lg-4 col-md-4 col-12'>
+            <div className='projectImage'> 
+              <img src ={school} alt="" className='img-fluid projectImage1'/>
+            </div>
+            </div>
+            <div className='col-xl-8 col-lg-8 col-md-8 col-12'>
+            <div className='projectText'>
+              <h3>School Fees Management System.</h3>
+              <h4>React JS - Redux - MUI -Node Js - Express- MongoDB - Javascript</h4>
+              <p>A School Fees Management System using React, Redux, MUI, Node.js, Express, and MongoDB allows schools to manage student accounts, fees, and complaints efficiently. Admins can add, update, and view fees for individual students, classes, or sections, while students can view their fees and raise complaints. Admins can view and respond to student complaints through a streamlined interface. The system also includes circular management, enabling the admin to send circulars to specific students, classes, or the entire school. This system ensures smooth interaction between students and the school, with secure authentication and user management via JWT.</p>
+            </div>
+            <div className='arrowPostion'>
+
+            <a target='blank' href='https://github.com/Mari-2003/Student_Management'><FaArrowAltCircleRight className='image-fluid projectArrow'/></a>
             </div>
           </div>
         </div>
